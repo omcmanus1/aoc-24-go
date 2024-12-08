afd9b4bdc51ef898a4415c3780576f76d1c8b7d5
omcmanus1 <omcmanus1@gmail.com>
2024-12-08 17:23:01 +0000

    DAY 1

---
+++ day1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func OneOne() {
+	listOne, listTwo := buildLists("day1input.txt")
+	sortListDescending(listOne)
+	sortListDescending(listTwo)
+	var totalDifference int
+	for i := range listOne {
+		difference := int(math.Abs(float64(listOne[i] - listTwo[i])))
+		totalDifference += difference
+	}
+	fmt.Println("Total Difference: ", totalDifference)
+}
+
+func OneTwo() {
+	listOne, listTwo := buildLists("day1input.txt")
+	similarityScore := 0
+	for _, listOneVal := range listOne {
+		instanceCount := 0
+		for _, listTwoVal := range listTwo {
+			if listOneVal == listTwoVal {
+				instanceCount++
+			}
+		}
+		similarityScore += listOneVal * instanceCount
+	}
+	fmt.Println("Similarity Score: ", similarityScore)
+}
+
+func buildLists(path string) ([]int, []int) {
+	scanner, file := GetFileScanner(path)
+	defer file.Close()
+	var listOne []int
+	var listTwo []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		locationIds := strings.Split(line, "   ")
+		firstId, _ := strconv.Atoi(locationIds[0])
+		secondId, _ := strconv.Atoi(locationIds[1])
+		listOne = append(listOne, firstId)
+		listTwo = append(listTwo, secondId)
+	}
+	return listOne, listTwo
+}
+
+func sortListDescending(list []int) {
+	sort.Slice(list, func(i, j int) bool {
+		return list[i] > list[j]
+	})
+}
